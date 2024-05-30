d7113bbadc7d81c1d864c32d26fcfb8f81de59a0
Evgeniy <jonhespeto@gmail.com>
2024-05-30 17:16:00 +0000

    commit

--- jenkins/web_jenkins.groovy
+++ jenkins/web_jenkins.groovy
@@ -1,23 +1,56 @@
 #!groovy
-// Check properties
+
 properties([disableConcurrentBuilds()])
 
 pipeline {
     agent any
     options {
-        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
         timestamps()
     }
     stages {
-        stage('First step') {
+        stage('Checkout code') {
             steps {
-                sh 'hostname'
+                git 'https://github.com/jonhespeto/web.git'
             }
         }
-        stage('Second step') {
+        stage('Modify HTML') {
             steps {
-                sh 'uptime'
+                script {
+                    def htmlContent = readFile('index.html')
+                    htmlContent = htmlContent.replaceAll('<!-- TITLE -->', web_TITLE)
+                                             .replaceAll('<!-- CONTENT_1 -->', web_CONTENT_1)
+                                             .replaceAll('<!-- CONTENT_2 -->', web_CONTENT_2)
+                                             .replaceAll('<!-- CONTENT_3 -->', web_CONTENT_3)
+                                             .replaceAll('<!-- CONTENT_4 -->', web_CONTENT_4)
+                                             .replaceAll('<!-- CONTENT_5 -->', web_CONTENT_5)
+                                             .replaceAll('<!-- CONTENT_6 -->', web_CONTENT_6)
+                                             .replaceAll('<!-- CONTENT_7 -->', web_CONTENT_7)
+                    writeFile(file: 'index.html', text: htmlContent)
+                }
             }
         }
+        stage('Show Modified Page') {
+            steps {
+                script {
+                    def modifiedHtmlContent = readFile('index.html')
+                    echo "Modified HTML page content:n${modifiedHtmlContent}"
+                }
+            }
+        }
+        // stage('Deploy to server') {
+        //     steps {
+        //         sshagent([SSH_KEY_CREDENTIALS_ID]) {
+        //             sh "scp -r * ${SERVER_USER}@${SERVER_HOST}:${DEPLOY_DIRECTORY}"
+        //         }
+        //     }
+        // }
+        // stage('Restart NGINX') {
+        //     steps {
+        //         sshagent([SSH_KEY_CREDENTIALS_ID]) {
+        //             sh "ssh ${SERVER_USER}@${SERVER_HOST} 'sudo nginx -s reload'"
+        //         }
+        //     }
+        // }
     }
 }
