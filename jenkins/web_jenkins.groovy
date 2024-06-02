9f700c8b28eccdd83da44ffc4b6fb8c0d6a21c7d
Evgeniy <jonhespeto@gmail.com>
2024-06-02 14:03:29 +0000

    commit

--- jenkins/web_jenkins.groovy
+++ jenkins/web_jenkins.groovy
@@ -13,18 +13,18 @@ pipeline {
         timestamps()
     }
     stages {
-        // 
+        //
         stage('Checkout code') {
-            steps {
-                git branch: 'main', url: 'https://github.com/jonhespeto/web.git'
-            }
+      steps {
+        git branch: 'main', url: 'https://github.com/jonhespeto/web.git'
+      }
         }
-        // 
+        //
         stage('Modify index.html') {
-            steps {
-                script {
-                    def htmlContent = readFile('index.html')
-                    htmlContent = htmlContent.replaceAll('<!-- TITLE -->', web_TITLE)
+      steps {
+        script {
+          def htmlContent = readFile('index.html')
+          htmlContent = htmlContent.replaceAll('<!-- TITLE -->', web_TITLE)
                                              .replaceAll('<!-- CONTENT_1 -->', web_CONTENT_1)
                                              .replaceAll('<!-- CONTENT_2 -->', web_CONTENT_2)
                                              .replaceAll('<!-- CONTENT_3 -->', web_CONTENT_3)
@@ -33,54 +33,54 @@ pipeline {
                                              .replaceAll('<!-- CONTENT_6 -->', web_CONTENT_6)
                                              .replaceAll('<!-- CONTENT_7 -->', web_CONTENT_7)
                                              .replaceAll('<!-- CONTENT_8 -->', web_CONTENT_8)
-                    writeFile(file: 'index.html', text: htmlContent)
-                }
-            }
+          writeFile(file: 'index.html', text: htmlContent)
         }
-        // 
+      }
+        }
+        //
         stage('Show modified page') {
-            steps {
-                script {
-                    def modifiedHtmlContent = readFile('index.html')
-                    echo "Modified HTML page content:n${modifiedHtmlContent}"
-                }
-            }
+      steps {
+        script {
+          def modifiedHtmlContent = readFile('index.html')
+          echo "Modified HTML page content:n${modifiedHtmlContent}"
+        }
+      }
         }
         // Build docker image and tests z
         stage('Build and run docker') {
-            steps {
-                script {
-                    sh "docker build -t ${DOCKER_IMAGE} ."
-                    sh "docker run -d --name ${CONTAINER_NAME} -p 8888:80 ${DOCKER_IMAGE}"
-                    sh "sleep 5"
-                    sh "curl --fail http://localhost:8888/index.html || exit 1"
-                    sh "curl http://localhost:8888 | grep '${web_TITLE}' || exit 1"
-                    sh 'curl -s http://localhost:8888 | grep "<img src=" | grep -oP "src="K[^"]+" | xargs -I {} curl --fail -o /dev/null {} || exit 1'
-                }
-            }
+      steps {
+        script {
+          sh "docker build -t ${DOCKER_IMAGE} ."
+          sh "docker run -d --name ${CONTAINER_NAME} -p 8888:80 ${DOCKER_IMAGE}"
+          sh 'sleep 5'
+          sh 'curl --fail http://localhost:8888/index.html || exit 1'
+          sh "curl http://localhost:8888 | grep '${web_TITLE}' || exit 1"
+          sh 'curl -s http://localhost:8888 | grep "<img src=" | grep -oP "src="K[^"]+" | xargs -I {} curl --fail -o /dev/null {} || exit 1'
         }
-        // Replace file index.html on server
+      }
+        }
+        // Replace file and copy to server
         stage('Replace index.html') {
-            when {
-                expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-            }
-            steps {
-                sshagent(['ssh_key_for_nginx']) {
-                    sh "scp -P ${SERVER_PORT} -o StrictHostKeyChecking=no index.html ${SERVER_USER}@${SERVER_HOST}:${SERVER_FROM_DIRECTORY}/"
-                    sh "ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} 'sudo cp ${SERVER_FROM_DIRECTORY}/index.html ${DEPLOY_DIRECTORY}/index.html'"
-                    sh "ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} 'sudo nginx -s reload'"
-                }
-            }
+      when {
+        expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
+      }
+      steps {
+        sshagent(['ssh_key_for_nginx']) {
+          sh "scp -P ${SERVER_PORT} -o StrictHostKeyChecking=no * ${SERVER_USER}@${SERVER_HOST}:${SERVER_FROM_DIRECTORY}/"
+          sh "ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} 'sudo cp ${SERVER_FROM_DIRECTORY}/* ${DEPLOY_DIRECTORY}/'"
+          sh "ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} 'sudo nginx -s reload'"
+        }
+      }
         }
     }
     // Removes esting docker container and image
     post {
         always {
-            script {
-                sh "docker stop ${CONTAINER_NAME}"
-                sh "docker rm ${CONTAINER_NAME}"
-                sh "docker rmi ${DOCKER_IMAGE}"
-            }
+      script {
+        sh "docker stop ${CONTAINER_NAME}"
+        sh "docker rm ${CONTAINER_NAME}"
+        sh "docker rmi ${DOCKER_IMAGE}"
+      }
         }
     }
 }
